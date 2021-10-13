f5cc3fdfc07bc71468b1afe11d339ae54eda8a25
m-r-ebrahimi <emohammadreza568@gmail.com>
2021-10-13 22:34:04 +0330

    Bbounded example.

--- src/main/java/Q2/Bounded.java
+++ src/main/java/Q2/Bounded.java
@@ -1,4 +1,24 @@
 package Q2;
 
-public class Bounded {
+public class Bounded<T> {
+    private T t;
+
+    public void set(T t) {
+        this.t = t;
+    }
+
+    public T get() {
+        return t;
+    }
+
+    public <U extends Number> void inspect(U u){
+        System.out.println("T: " + t.getClass().getName());
+        System.out.println("U: " + u.getClass().getName());
+    }
+
+    public static void main(String[] args) {
+        Bounded<Integer> integerBox = new Bounded<Integer>();
+        integerBox.set(new Integer(10));
+        integerBox.inspect("some text"); // error: this is still String!
+    }
 }
