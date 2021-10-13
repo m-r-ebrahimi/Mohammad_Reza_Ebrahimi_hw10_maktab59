79cad662b83bc7999bbc5b8ee1504c043d418624
m-r-ebrahimi <emohammadreza568@gmail.com>
2021-10-13 22:26:38 +0330

    Unbounded example.

--- src/main/java/Q2/Unbounded.java
+++ src/main/java/Q2/Unbounded.java
@@ -4,16 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Unbounded {
-    public static void main(String[] args) throws ClassNotFoundException
-    {
+    public static void main(String[] args) {
         List<Object> objects = new ArrayList<>();
         objects.add(new Object());
+        objects.add(null);
         objects.add(new Person("Don Draper", 89));
-        System.out.println(objects);
-
-
+        printList(objects);
         List<?> wildcards = new ArrayList<>();
         wildcards.add(null);
-        System.out.println(wildcards);
+        printListNew(wildcards);
+    }
+
+    public static void printList(List<Object> list) {
+        for (Object elem : list)
+            System.out.println(elem + " ");
+        System.out.println();
+    }
+    public static void printListNew(List<?> list) {
+        for (Object elem: list)
+            System.out.print(elem + " ");
+        System.out.println();
     }
 }
