1cf25b1d73864fb51a0ae7f440a2dd8504ce06cb
m-r-ebrahimi <emohammadreza568@gmail.com>
2021-10-13 22:26:47 +0330

    Upperbounded example.

---
+++ src/main/java/Q2/UpperBounded.java
@@ -0,0 +1,20 @@
+package Q2;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class UpperBounded {
+    public static void main(String[] args) {
+        List<Integer> li = Arrays.asList(1, 2, 3);
+        System.out.println("sum = " + sumOfList(li));
+        List<Double> ld = Arrays.asList(1.2, 2.3, 3.4);
+        System.out.println("sum = " + sumOfList(ld));
+    }
+
+    public static double sumOfList(List<? extends Number> list) {
+        double s = 0.0;
+        for (Number n : list)
+            s += n.doubleValue();
+        return s;
+    }
+}
